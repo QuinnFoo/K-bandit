6ab5642d900a282b6b5ac4c5d27f0e19a1f974bf
QuinnFoo <67194945+QuinnFoo@users.noreply.github.com>
2021-03-02 00:11:47 +0800

    rip

--- HW1.py
+++ HW1.py
@@ -1,101 +1,124 @@
-#Some intuition:  we need:
-# 1. collect and process the stock data, distribute certain amount of money (maybe $100) everyday
-# 2. DEFINE State: the current stock amount? current price
-# 3. DEFINE Action: sell, buy, hold with amount Weight[-1, 1]
-# 4. DEFINE Reward(a function with step t): let's try maximum the return first
-# 5. DEFINE Agent: take one algorithm(epsilon greedy ?), value estimation ,choose action based on State
-# 6. Run Training (like 1000 epochs)
-##################################################################################################################
-
+##########################################################################################
+# Idea: K-arms are stocks.
+# Action is picking a certain stock to invest everyday.
+# Invest method: buy at open, sell at close.
+# reward: daily return.
+##########################################################################################
 import math
 import numpy as np
 import pandas as pd
 
-#steps: t
-t=1000
 
-#TODO: stockList: a dataframe of stocks for us to invest
-stockList = pd.DataFrame()
-actionCount = len(stockList.colomns)
+df = pd.read_csv('K-bandit/PandasNumpy.csv', index_col=0, header=[0, 1])
+index = df.columns.get_level_values(1)[0:10]
 
-#stock: time(date), open, close,
-def dailyReturn(stock, t, money=100):
-    open, close = stockList[stock][t]
-    shares = money/open
-    return shares*close - money
+openList = df.iloc[:, 30:40]
+openList.columns = index
 
-#we call rewardFun() to compute reward each step and action for us.
-#I don't use dailyReturn as rewardFun is that maybe we can transfer it to other form, like dropdown, later.
-def rewardFun(action, t):
-    return dailyReturn(action, t)
+closeList = df.iloc[:, 10:20]
+closeList.columns = index
+
+# stock name list, also the choice of action
+ActionCount = len(openList.columns)
+
+t = len(openList)
+
+
+# stock: time(date), open, close,
+def rewardReturn(t, stock, money=100):
+    open = openList[t][stock]
+    close = closeList[t][stock]
+    shares = money / open
+    return shares * close - money
+
 
 ############################## ALGORITHM PART #############################################
-#actionCount is the total number of action(0,1,2..)
+# actionCount is the total number of action(0,1,2..)
 
-def eps_Greedy(epsilon,rewardFun,actionCount):
-    reward = [0.0]*actionCount
-    Qta = [0.0]*actionCount
-    NumAction=[1] * actionCount
-    p=np.random.rand(actionCount)
+def eps_Greedy(eps, step):
+    p = np.random.rand(ActionCount)
 
-    for i in range(1,t):
-        if p <= epsilon:
-            Action = np.random.choice(actionCount)
-        else:
-            Action = reward.index(max(Qta))
+    if p <= eps:
+        action = np.random.choice(ActionCount)
+    else:
+        action = reward.index(max(Qta))
 
-        #update
-        NumAction[Action] += 1
-        reward[Action] += rewardFun(i,Action)
-        Qta[Action] += reward[Action]/NumAction[Action]
+    # update
+    NumAction[action] += 1
+    reward[action] += rewardReturn(step, action)
+    Qta[action] += reward[action] / NumAction[action]
 
-        yield Action, reward[Action]
+    return reward[action]
 
-#make epsilon decay: epsilon=1/(1+t/actionCount)
-def decayEps_Greedy(rewardFun, actionCount):
-    reward = [0.0]*actionCount
-    Qta = [0.0]*actionCount
-    NumAction=[1] * actionCount
-    p=np.random.rand(actionCount)
 
-    for i in range(1,t):
-        if p <= 1/(1+i/actionCount):
-            Action = np.random.choice(actionCount)
-        else:
-            Action = reward.index(max(Qta))
+# make epsilon decay: epsilon=1/(1+t/actionCount)
+def decayEps_Greedy(step):
+    p = np.random.rand(ActionCount)
 
-        #update
-        NumAction[Action] += 1
-        reward[Action] += rewardFun(i,Action)
-        Qta[Action] += reward[Action]/NumAction[Action]
+    if p <= 1 / (1 + step / ActionCount):
+        action = np.random.choice(ActionCount)
+    else:
+        action = reward.index(max(Qta))
 
-        yield Action, reward[Action]
+    # update
+    NumAction[action] += 1
+    reward[action] += rewardReturn(step, action)
+    Qta[action] += reward[action] / NumAction[action]
 
-def UCB(rewardFun, actionCount, c=2):
-    #for each action, initialize:
-    #NumAction(Nt(a)) is the times each action has been done
-    NumAction= [1] * actionCount
-    Qta= [0.0] * actionCount
-    reward= [0.0] * actionCount
+    return reward[action]
 
-    #ucbF is that [] in max formula to chose At, we want the Action At with max ucbF
-    #initialize ucbF with 0
-    ucbF= [0.0] * actionCount
 
-    for i in range(1,t):
-        #count ucbF for each action every step
-        for j in range(NumAction):
-            ucbF[j]= Qta[j] + c * math.sqrt(math.log(i)/NumAction[j])
+def UCB(step, c=2):
+    # NumAction(Nt(a)) is the times each action has been done
+    # ucbF is that [] in max formula to chose At, we want the Action At with max ucbF
+    # initialize ucbF with 0
 
-        #choose the Action index whose ucbF is max
-        Action= ucbF.index(max(ucbF))
-        NumAction[Action] += 1
-        reward[Action] += rewardFun(i,Action)
-        Qta[Action] += reward[Action]/NumAction[Action]
+    # count ucbF for each action every step
+    for j in range(ActionCount):
+        ucbF[j] = Qta[j] + c * math.sqrt(math.log(step) / NumAction[j])
 
-        yield Action, reward[Action]
+        # choose the Action index whose ucbF is max
+    action = ucbF.index(max(ucbF))
+    NumAction[action] += 1
+    reward[action] += rewardReturn(step, action)
+    Qta[action] += reward[action] / NumAction[action]
+
+    return reward[action]
+
+
 ############################## ALGORITHM PART #############################################
+# initialization
+traceReward1 = []
+traceReward2 = []
+traceReward3 = []
 
-#TODO: visualize the reward
-#git test
-#git 2nd test
+############################epsilon greedy result#################
+epsilon = 0.9
+reward = [0.0] * ActionCount
+Qta = [0.0] * ActionCount
+NumAction = [1] * ActionCount
+
+for i in range(t):
+    r = eps_Greedy(epsilon, i)
+    traceReward1.append(r)
+
+############################greedy decay result###################
+epsilon = 0.9
+reward = [0.0] * ActionCount
+Qta = [0.0] * ActionCount
+NumAction = [1] * ActionCount
+
+for i in range(t):
+    r = decayEps_Greedy(t)
+    traceReward2.append(r)
+
+###########################UCB result##################
+epsilon = 0.9
+reward = [0.0] * ActionCount
+Qta = [0.0] * ActionCount
+NumAction = [1] * ActionCount
+ucbF = [0.0] * ActionCount
+
+for i in range(t):
+    r = UCB(t, c=2)
+    traceReward3.append(r)
