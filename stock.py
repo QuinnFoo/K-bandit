177ffc3a720db902e869666c930105ea86b0cc77
xyubi <79782527+xyubi@users.noreply.github.com>
2021-03-01 11:18:07 +0800

    Update stock.py

--- stock.py
+++ stock.py
@@ -5,29 +5,31 @@ import sys
 import pandas_datareader as pdr
 import string
 import random
+np.warnings.filterwarnings('ignore')
 
-# set up the time
-start = datetime.datetime(2021, 1, 1)
+#set up the time
+start = datetime.datetime(2018, 1, 1)
 end = datetime.datetime(2021, 1, 27)
 
-# pick 10 stocks in random
+#pick 10 stocks in random
 seeds = string.digits
 
-stocks = []
-while len(stocks) < 10:
+stocks=[]
+index=web.DataReader("0001.HK", "yahoo", start, end).index[0]
+while len(stocks)<10:
     random_str = []
     for i in range(4):
         random_str.append(random.choice(seeds))
     random_str.append('.HK')
-
-    try:
-        web.DataReader("".join(random_str), "yahoo", start, end)
-        stocks.append("".join(random_str))
+    stock_inf=[]
+    try: 
+        stock_inf=web.DataReader("".join(random_str), "yahoo", start, end)
+        if((stock_inf.ix[0,0]!=None) & (stock_inf.index[0]==index)):
+               stocks.append("".join(random_str))
     except:
         continue
 
 print(stocks)
-
 #reload the all information of the stocks
 
 stockList = web.DataReader(stocks, "yahoo", start, end)
